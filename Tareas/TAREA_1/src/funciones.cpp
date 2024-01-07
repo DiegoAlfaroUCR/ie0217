4679b3f94bcf9ea51ecba28afae4b3cd2de70b68
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-01-06 20:07:40 -0600

    Tarea 1, funciones de random, temperatura y demás agregadas.
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- Tareas/TAREA_1/src/funciones.cpp
+++ Tareas/TAREA_1/src/funciones.cpp
@@ -21,10 +21,87 @@ int mostrarMenu(){
 
 void procesarOpcion(int limInferior, int limSuperior, int opcion){
     // Se establece la cantidad máxima de intentos y un contador de intentos.
-    const int MAX_INTENTOS = (limSuperior - limInferior)/3;
+    int tamañoIntervalo = abs(limSuperior - limInferior);
+    const int MAX_INTENTOS = tamañoIntervalo/3;
     int intentos = 0;
+
+    int buscado = numRandom(limInferior, limSuperior);
+
+    while (intentos < MAX_INTENTOS)
+    {
+        int guess;
+        cout << "Adivine un numero: ";
+        cin >> guess;
+
+        if (guess == buscado){
+            mensajeGanador();
+        } else {
+            pista(guess, buscado, opcion, tamañoIntervalo);
+            ++intentos;
+        }
+        
+    };
+
+    mensajePerdedor();
 }
 
+
+int numRandom(int limInferior, int limSuperior){
+    srand(time(0));
+    return ((rand() % limSuperior) + limInferior); // Se tomó referencia para este método de: https://shorturl.at/rKOPT
+}
+
+
+void pista(int guess, int buscado, int opcion, int tamañoIntervalo){
+    switch (opcion)
+    {
+    case 1:
+        if (guess<buscado)
+        {
+            cout << "El numero buscado es mayor." << endl;
+        } else {
+            cout << "El numero buscado es menor." << endl;}
+        break;
+    
+    case 2:
+        string temperatura = calcularTemperatura(tamañoIntervalo, guess, buscado);
+        cout << "Su numero anda: " << temperatura << endl;
+        break;
+    }
+}
+
+string calcularTemperatura(int tamañoIntervalo, int guess, int buscado){
+    /*
+    Si se divide el tamaño del intervalo en 7 secciones, se puede tomar la seccion 
+    que contiene al numero guess como "hirviendo", la siguiente mas cercana como caliente 
+    y al resto como frio y congelado sucesivamente. Esto evidentemente es una división que
+    toma en cuenta cada lado del numero. Para esto se divide el intervalo en 7. 
+    Se busca encontrar si distancia/rangoTemperatura < 0.5, entonces pertenece a hirviente, 
+    esto porque el intervalo hirviente tiene al numero en su centro.
+
+    Ej: Sea x el numero buscado y z el guess;
+    congelado--frio--caliente--hirviendo--caliente--frio--congelado
+                                   x        z
+
+    En este caso z se encontraria en la posicion caliente. Y rangoTemperatura<distancia<(1.5)*rangoTemperatura.
+    */
+    float distancia = abs(buscado - guess);
+    float rangoTemperatura = float(tamañoIntervalo)/7;
+    float posicion = distancia/rangoTemperatura;
+
+    if (posicion <= 0.5)
+    {
+        return "hirviendo";
+    } else if (posicion <= 1.5)
+    {
+        return "caliente";
+    } else if (posicion <= 2.5)
+    {
+        return "frio";
+    } else {
+        return "congelado";
+    }
+}
 
 void mensajeGanador(){
     cout << "VICTORIA: Felicidades ha adivinado el numero!!" << endl;
--- Tareas/TAREA_1/src/funciones.hpp
+++ Tareas/TAREA_1/src/funciones.hpp
@@ -4,10 +4,15 @@
 // Se incluyen librerías necesarias.
 #include <iostream>
 #include <string>
+#include <cstdlib> // Se incluye para generar un numero random.
+#include <ctime>  // Se incluye para crear numeros random diferentes en cada caso.
 using namespace std;
 
 int mostrarMenu();
 void procesarOpcion(int limInferior, int limSuperior, int opcion);
+int numRandom(int limInferior, int limSuperior);
+void pista(int guess, int buscado, int opcion, int tamañoIntervalo);
+string calcularTemperatura(int tamañoIntervalo, int guess, int buscado);
 void mensajeGanador();
 void mensajePerdedor();
 
