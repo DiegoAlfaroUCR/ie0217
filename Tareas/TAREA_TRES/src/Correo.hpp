4dd02edcb4a62502356036df108c5e3d39c7856f
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-01-22 21:02:06 -0600

    Tarea 3, Parte Práctica: Expresión Regular.
    Se creó la Expresión Regular con regex para la parte 2.
    
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- Tareas/TAREA_TRES/src/Correo.hpp
+++ Tareas/TAREA_TRES/src/Correo.hpp
@@ -9,7 +9,7 @@ using namespace std;
 
 class Correo {
     private:
-        const regex expresion = regex("a@");
+        const regex expresion = regex("^(?![\\.\\-\\_])[a-zA-Z0-9\\.\\-\\_]{0,14}[a-zA-Z0-9]@[a-zA-Z]+\\.[a-zA-Z]{2,4}$");
         const regex arroba = regex("@");
     public:
         bool verificar();
