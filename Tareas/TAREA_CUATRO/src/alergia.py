3081181eae90dfde3b0ec5c76bed7970eb7ee842
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-01-26 15:54:07 -0600

    Tarea 4, Parte Práctica: Clase Alergia
    Se creó la clase Alergia.
    
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

---
+++ Tareas/TAREA_CUATRO/src/alergia.py
@@ -0,0 +1,13 @@
+
+class Alergia:
+    def __init__(self, name, puntos):
+        self.name = name
+        self.puntos = puntos
+
+    def mostrar_info(self, todasAlergias=None):
+        if todasAlergias is not None:
+            print("-----Mostrando informacion de todas las alergias-----\n")
+            for alergia in todasAlergias:
+                alergia.mostrar_info()
+        else:
+            print(f"Nombre: {self.name}, Puntuacion: {self.puntos}\n")
