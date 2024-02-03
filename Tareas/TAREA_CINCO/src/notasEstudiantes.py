8813c6501bb75b6cb4453d12b07a539269f39fd1
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-02-02 23:36:03 -0600

    Tarea 5, Parte Práctica 2: Impresión de resultados.
    Se agregó el cálculo de la suma de notas y se creó el script para imprimir la informacion de cada estudiante en un buen formato. Además se estandarizó la cantidad de estudiantes, asignaturas y notas por medio de constantes globales en el caso de que se quiera expandir el programa.
    
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- Tareas/TAREA_CINCO/src/notasEstudiantes.py
+++ Tareas/TAREA_CINCO/src/notasEstudiantes.py
@@ -1,33 +1,67 @@
 import numpy as np
 
-# asignaturas = ["Fisica", "Matematica", "Quimica", "Biologia", "Español"]
+asignaturas = ["Fisica", "Matematica", "Quimica", "Biologia", "Español"]
 
-ASIGNATURAS = NUM_ESTUDIANTES = 5
+NUM_ASIGNATURAS = 5
+NUM_ESTUDIANTES = 5
+NUM_NOTAS = 5
 
 
 class Estudiante():
-    calificaciones = np.empty((5, 5))
-    promedios = np.empty((5, 1))
+    calificaciones = np.empty((NUM_ASIGNATURAS, NUM_NOTAS))
 
     def __init__(self):
         self.crearCalificaciones()
-        self.calcularPromedios()
 
     def crearCalificaciones(self):
-        for asignatura in range(ASIGNATURAS):
+        for asignatura in range(NUM_ASIGNATURAS):
             notas = np.random.randint(0, 100, 5)
             self.calificaciones[asignatura] = notas
 
-    def calcularPromedios(self):
-        for asignatura in range(ASIGNATURAS):
-            promedio = np.mean(self.calificaciones[asignatura])
-            self.promedios[asignatura] = promedio
+
+def calcularPromedios(calificaciones):
+    promedios = np.empty((NUM_ASIGNATURAS, 1))
+    for asignatura in range(NUM_ASIGNATURAS):
+        promedio = np.mean(calificaciones[asignatura])
+        promedios[asignatura] = promedio
+    return promedios
 
 
-notasEstudiantes = np.empty((5, 5, 5))
-promediosEstudiantes = np.empty((5, 5, 1))
+def calcularMaxima(calificaciones):
+    max = 0
+    for notas in calificaciones:
+        if np.max(notas) > max:
+            max = np.max(notas)
+    return max
+
+
+def sumaNotas(calificaciones):
+    sumas = np.empty((NUM_ASIGNATURAS, 1))
+    for asignatura in range(NUM_ASIGNATURAS):
+        suma = np.sum(calificaciones[asignatura])
+        sumas[asignatura] = suma
+    return sumas
+
+
+notasEstudiantes = np.empty((NUM_ESTUDIANTES, NUM_ASIGNATURAS, NUM_NOTAS))
+promediosEstudiantes = np.empty((NUM_ESTUDIANTES, NUM_ASIGNATURAS, 1))
+maximasEstudiantes = np.empty((NUM_ESTUDIANTES, 1))
+sumasEstudiantes = np.empty((NUM_ESTUDIANTES, NUM_ASIGNATURAS, 1))
 
 for i in range(NUM_ESTUDIANTES):
     unEstudiante = Estudiante()
     notasEstudiantes[i] = unEstudiante.calificaciones
-    promediosEstudiantes[i] = unEstudiante.promedios
+    promediosEstudiantes[i] = calcularPromedios(notasEstudiantes[i])
+    maximasEstudiantes[i] = calcularMaxima(notasEstudiantes[i])
+    sumasEstudiantes[i] = sumaNotas(notasEstudiantes[i])
+
+print("-----Se muestra información de cada estudiante-----")
+for estudiante in range(NUM_ESTUDIANTES):
+    print('\n'+"-"*70, f"\nEstudiante {estudiante + 1}:")
+    print(f"Calificaciones por materia: {'':9s} Promedios:    Suma de notas:")
+    for i in range(NUM_ASIGNATURAS):
+        print(f"{asignaturas[i]:11s}: {notasEstudiantes[estudiante][i]}",
+              f"{'':3s}{promediosEstudiantes[estudiante][i]}",
+              f"{'':7s}{sumasEstudiantes[estudiante][i]}")
+    print(f"\nNota máxima obtenida: {maximasEstudiantes[estudiante]}")
+    print("-"*70)
