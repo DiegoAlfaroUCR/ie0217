b363b8c4f692fb25a633d1fbb87a5a030698bb46
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-02-10 22:27:09 -0600

    Tarea 6, Parte Práctica: Correción regresiones.
    Se corrige la forma en la que se genera las regresiones del método precioXaño para resolver problemas de graficación.
    
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- Tareas/TAREA_SEIS/cars_analysis/regression.py
+++ Tareas/TAREA_SEIS/cars_analysis/regression.py
@@ -9,12 +9,15 @@ def precioXaño(datos):
     # Se extraen los datos del DataFrame.
     datos = datos.sort_values(by=["year"])
     xRaw = datos["year"].values.reshape(-1, 1)
-    print(xRaw)
     yRaw = datos["selling_price"].values
 
     # Se crean los datos train y test.
     X_train, X_test, y_train, y_test = train_test_split(
         xRaw, yRaw, test_size=0.2, random_state=42)
+
+    # Se ordena el eje X de prueba, pues este se desordena y causa
+    # problemas en el modelo polinomial.
+    X_test.sort(axis=0)
 
     # Se hace la regresión lineal
     lineal = LinearRegression()
@@ -22,14 +25,20 @@ def precioXaño(datos):
     yLineal = lineal.predict(X_test)
 
     # Se hace la regresión no lineal
-    degree = 2
+    degree = 4
     noLineal = make_pipeline(PolynomialFeatures(degree), LinearRegression())
     noLineal.fit(X_train, y_train)
     yNoLineal = noLineal.predict(X_test)
 
+    # Se muestran todos los puntos.
     plt.scatter(xRaw, yRaw)
+
+    # Se muestran las regresiones.
     plt.plot(X_test, yLineal, c="Red", label="Regresión lineal.")
-    plt.plot(X_test, yNoLineal, c="Green", label="Regresión no lineal.")
+    plt.plot(X_test, yNoLineal, c="Green",
+             label=f"Regresión polinomica grado {degree}.")
+
+    # Personalización
     plt.title("Precio en función del año.")
     plt.xlabel("Año.")
     plt.ylabel("Precio.")
