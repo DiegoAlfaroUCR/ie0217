1d2ce427a03c66a3b87c75cb0fa96c64d6869bba
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-02-11 18:07:13 -0600

    Tarea 6, Parte Práctica: Makefile
    Se creó el Makefile usado para iniciar el programa. Además, se cambió la estructura de main y generarCSV para incluir la función obtenerDatos en generarCSV.
    
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- Tareas/TAREA_SEIS/cars_analysis/generarCSV.py
+++ Tareas/TAREA_SEIS/cars_analysis/generarCSV.py
@@ -2,11 +2,12 @@ import requests
 import os
 from requests.auth import HTTPBasicAuth
 from zipfile import ZipFile
+import pandas as pd
 
 
 def generarCSV():
     # Se sale si el csv ya existe.
-    if os.path.isfile("CAR DETAILS FROM CAR DEKHO.csv"):
+    if os.path.isfile("cars_analysis\\CAR DETAILS FROM CAR DEKHO.csv"):
         return
 
     # Credenciales
@@ -39,7 +40,25 @@ def generarCSV():
 
     # Se extrae el archivo.
     with ZipFile(path, 'r') as zip_ref:
-        zip_ref.extractall()
+        zip_ref.extractall("cars_analysis")
 
     # Se remueve el .zip
     os.remove(path)
+
+
+def obtenerDatos():
+    # Se genera el CSV si no existe.
+    generarCSV()
+
+    # Se leen los datos del csv descargado.
+    raw = pd.read_csv("cars_analysis\\CAR DETAILS FROM CAR DEKHO.csv")
+
+    # Se sacan categorias útiles.
+    utiles = raw[["selling_price", "year", "km_driven"]]
+
+    # Se limpian valores NaN/Nulos.
+    limpios = utiles[utiles["selling_price"].notna() |
+                     utiles["year"].notna() |
+                     utiles["km_driven"].notna()]
+
+    return limpios
--- Tareas/TAREA_SEIS/cars_analysis/main.py
+++ Tareas/TAREA_SEIS/cars_analysis/main.py
@@ -1,25 +1,8 @@
-import pandas as pd
-from generarCSV import generarCSV
+from generarCSV import obtenerDatos
 from regression import precioXaño, kilometrajeXaño
 from clustering import generarClusters
 
 
-def obtenerDatos():
-    # Se leen los datos del csv descargado.
-    raw = pd.read_csv("CAR DETAILS FROM CAR DEKHO.csv")
-
-    # Se sacan categorias útiles.
-    utiles = raw[["selling_price", "year", "km_driven"]]
-
-    # Se limpian valores NaN/Nulos.
-    limpios = utiles[utiles["selling_price"].notna() |
-                     utiles["year"].notna() |
-                     utiles["km_driven"].notna()]
-
-    return limpios
-
-
-generarCSV()
 datos = obtenerDatos()
 precioXaño(datos)
 kilometrajeXaño(datos)
